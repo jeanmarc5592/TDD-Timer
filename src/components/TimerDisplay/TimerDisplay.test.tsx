78b0613b7f56d212fd7c7c19511a24ee8a157e20
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-02-12 14:23:54 +0100

    Add Test to show elapsed time

--- src/components/TimerDisplay/TimerDisplay.test.tsx
+++ src/components/TimerDisplay/TimerDisplay.test.tsx
@@ -34,4 +34,16 @@ describe("TimerDisplay", () => {
         expect(screen.getByText(/minutes/i)).toBeInTheDocument();
         expect(screen.getByText(/seconds/i)).toBeInTheDocument();
     });
+
+    it("shows elapsed time", () => {
+        const TimerDisplayProps = {
+            hours: "00",
+            minutes: "11",
+            seconds: "45"
+        };
+        render(<TimerDisplay {...TimerDisplayProps} />);
+        expect(screen.getByTestId("timer-hours").innerHTML).toBe("00");
+        expect(screen.getByTestId("timer-minutes").innerHTML).toBe("11");
+        expect(screen.getByTestId("timer-seconds").innerHTML).toBe("45");
+    })
 });
